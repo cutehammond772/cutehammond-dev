b3d83b8228410cdd033f6f4c167fa3f3ac50c92d
Jungheon Lee <cutehammond772@gmail.com>
2025-04-24 19:27:11 +0900

    refactor: Theme 로직을 next-themes 기반으로 변경
    - 기존 BlogProvider를 JotaiProvider 및 ThemeProvider로 분리
    - ThemeToggle 컴포넌트를 새로운 위치로 이동 및 수정
    - BlogContainer 및 Theme 관련 파일 삭제
    - globals.css에서 테마 관련 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import { pretendard } from "@/styles/font";
 import "@/styles/globals.css";
 
-import BlogContainer from "@/app/components/BlogContainer";
-import ThemePreloadScript from "@/theme/ThemePreloadScript";
 import ModalProvider from "@/modal/ModalProvider";
 
-import ThemeToggle from "./components/theme/ThemeToggle";
 import SettingsButton from "./components/settings/SettingsButton";
-import { Hash, Home } from "react-feather";
+import { Home } from "react-feather";
+import ThemeProvider from "./ThemeProvider";
+import ThemeToggle from "@/features/theme/components/ThemeToggle";
+import JotaiProvider from "./JotaiProvider";
 
 export const metadata: Metadata = {
   title: "cutehammond.dev",
@@ -28,48 +28,50 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function Layout({ children }: React.PropsWithChildren) {
   return (
-    <BlogContainer>
-      <html lang="ko" suppressHydrationWarning>
-        <body
-          className={`${pretendard.className} grid min-h-screen grid-cols-[0_1fr_0] grid-rows-[auto_1fr_auto] bg-beige-100 text-text-900 dark:bg-charcoal-900 dark:text-text-100 md:grid-cols-[1fr_4fr_1fr] lg:grid-cols-[1fr_2fr_1fr]`}
-        >
-          <ThemePreloadScript />
-          <ModalProvider />
+    <html lang="ko" suppressHydrationWarning>
+      <body
+        className={`${pretendard.className} bg-beige-100 text-text-900 dark:bg-charcoal-900 dark:text-text-100 grid min-h-screen grid-cols-[0_1fr_0] grid-rows-[auto_1fr_auto] md:grid-cols-[1fr_4fr_1fr] lg:grid-cols-[1fr_2fr_1fr]`}
+      >
+        <JotaiProvider>
+          <ThemeProvider
+            attribute="data-theme"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider />
 
-          {/* Header Section */}
-          <header className="sticky top-0 z-10 col-span-1 col-start-2 flex flex-row items-center justify-between bg-beige-100 p-4 shadow-2xl shadow-beige-100 dark:bg-charcoal-900 dark:shadow-charcoal-900 md:px-0 md:py-8">
-            <div className="flex flex-row items-center justify-center">
-              <Link href="/" className="f3-bold self-start">
-                🐹.dev
-              </Link>
-            </div>
-            <div className="absolute left-[50%] flex -translate-x-2/4 flex-row gap-2 md:gap-4">
-              <div className="fp-bold flex flex-row items-center justify-center gap-1 bg-beige-300 px-3 py-1 dark:bg-charcoal-700">
-                <Home className="p-1" />
-                <span className="hidden md:block">Home</span>
+            {/* Header Section */}
+            <header className="bg-beige-100 shadow-beige-100 dark:bg-charcoal-900 dark:shadow-charcoal-900 sticky top-0 z-10 col-span-1 col-start-2 flex flex-row items-center justify-between p-4 shadow-2xl md:px-0 md:py-8">
+              <div className="flex flex-row items-center justify-center">
+                <Link href="/" className="f3-bold self-start">
+                  🐹.dev
+                </Link>
               </div>
-            </div>
-            <div className="flex flex-row gap-2">
-              <ThemeToggle />
-              <SettingsButton />
-            </div>
-          </header>
+              <div className="absolute left-[50%] flex -translate-x-2/4 flex-row gap-2 md:gap-4">
+                <div className="fp-bold bg-beige-300 dark:bg-charcoal-700 flex flex-row items-center justify-center gap-1 px-3 py-1">
+                  <Home className="p-1" />
+                  <span className="hidden md:block">Home</span>
+                </div>
+              </div>
+              <div className="flex flex-row gap-2">
+                <ThemeToggle />
+                <SettingsButton />
+              </div>
+            </header>
 
-          {/* Main Section */}
-          <main className="col-span-1 col-start-2">{children}</main>
+            {/* Main Section */}
+            <main className="col-span-1 col-start-2">{children}</main>
 
-          {/* Footer Section */}
-          <footer className="fp-bold col-span-3 mt-24 gap-2 px-8 py-4 text-center">
-            Edit by Jungheon Lee
-          </footer>
-        </body>
-      </html>
-    </BlogContainer>
+            {/* Footer Section */}
+            <footer className="fp-bold col-span-3 mt-24 gap-2 px-8 py-4 text-center">
+              Edit by Jungheon Lee
+            </footer>
+          </ThemeProvider>
+        </JotaiProvider>
+      </body>
+    </html>
   );
 }
