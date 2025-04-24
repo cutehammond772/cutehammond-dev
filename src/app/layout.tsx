db042dc9d3ec63a958056494c591dd775bc85fc3
Jungheon Lee <cutehammond772@gmail.com>
2025-04-24 19:37:31 +0900

    refactor: 설정 관련 모달 및 버튼 컴포넌트 제거
    - SettingsButton, AuthModal, SettingsModal, ArticleRefreshOption, ListRefreshOption, Modal, ModalProvider 및 useModal 파일 삭제
    - Layout에서 ModalProvider 및 SettingsButton 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import Link from "next/link";
 import { pretendard } from "@/styles/font";
 import "@/styles/globals.css";
 
-import ModalProvider from "@/modal/ModalProvider";
-
-import SettingsButton from "./components/settings/SettingsButton";
 import { Home } from "react-feather";
 import ThemeProvider from "./ThemeProvider";
 import ThemeToggle from "@/features/theme/components/ThemeToggle";
@@ -41,9 +38,6 @@ export default async function Layout({ children }: React.PropsWithChildren) {
             enableSystem
             disableTransitionOnChange
           >
-            <ModalProvider />
-
-            {/* Header Section */}
             <header className="bg-beige-100 shadow-beige-100 dark:bg-charcoal-900 dark:shadow-charcoal-900 sticky top-0 z-10 col-span-1 col-start-2 flex flex-row items-center justify-between p-4 shadow-2xl md:px-0 md:py-8">
               <div className="flex flex-row items-center justify-center">
                 <Link href="/" className="f3-bold self-start">
@@ -58,14 +52,9 @@ export default async function Layout({ children }: React.PropsWithChildren) {
               </div>
               <div className="flex flex-row gap-2">
                 <ThemeToggle />
-                <SettingsButton />
               </div>
             </header>
-
-            {/* Main Section */}
             <main className="col-span-1 col-start-2">{children}</main>
-
-            {/* Footer Section */}
             <footer className="fp-bold col-span-3 mt-24 gap-2 px-8 py-4 text-center">
               Edit by Jungheon Lee
             </footer>
